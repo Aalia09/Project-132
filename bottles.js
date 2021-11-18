3dc9e2b3fc70321b1dc507a8ecb09fe57460ad01
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-11-18 14:07:27 +0530

    Add files via upload

---
+++ bottles.js
@@ -0,0 +1,31 @@
+img = "";
+statuss = "";
+
+function preload()
+{
+    img = loadImage("bottles.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    statuss = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error , results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+}
